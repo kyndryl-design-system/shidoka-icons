22586d11e68d9d846df145f8c15cb2fde8f7315d
Brian Patrick <brian.patrick3@kyndryl.com>
2024-07-22 16:23:31 -0500

    library search

--- src/stories/Library.stories.js
+++ src/stories/Library.stories.js
@@ -1,6 +1,8 @@
 import { html } from 'lit';
+import { useArgs } from '@storybook/preview-api';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import '@kyndryl-design-system/shidoka-foundation/components/card';
+import '@kyndryl-design-system/shidoka-applications/components/reusable/search';
 import Icons from '../manifest/icons.json';
 
 export default {
@@ -8,19 +10,37 @@ export default {
 };
 
 export const Library = {
-  render: () => {
+  args: {
+    icons: Icons,
+    searchTerm: '',
+  },
+  render: (args) => {
+    const [{ searchTerm }, updateArgs] = useArgs();
+
+    const handleInput = (e) => {
+      // console.log(e.detail.value);
+      updateArgs({ searchTerm: e.detail.value });
+      updateArgs({
+        icons: Icons.filter((icon) =>
+          icon.friendly_name.toLowerCase().includes(e.detail.value.toLowerCase())
+        ),
+      });
+    };
+
     return html`
+      <kyn-search .value=${args.searchTerm} @on-input=${(e) => handleInput(e)}>Search</kyn-search>
+
       <div class="icons">
-        ${Icons.map((Icon) => {
+        ${args.icons.map((icon) => {
           return html`
             <kd-card>
               <div class="icon">
-                <div class="icon-name kd-type--ui-04">${Icon.friendly_name}</div>
+                <div class="icon-name kd-type--ui-04">${icon.friendly_name}</div>
 
                 <div class="svg">
-                  ${Icon.duotone
-                    ? unsafeHTML(require(`../svg/duotone/${Icon.name}.svg`))
-                    : unsafeHTML(require(`../svg/monochrome/32/${Icon.name}.svg`))}
+                  ${icon.duotone
+                    ? unsafeHTML(require(`../svg/duotone/${icon.name}.svg`))
+                    : unsafeHTML(require(`../svg/monochrome/32/${icon.name}.svg`))}
                 </div>
               </div>
             </kd-card>
