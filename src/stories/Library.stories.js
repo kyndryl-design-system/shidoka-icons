ea2457d10221239ed793aae501931d7227bf401d
Brian Patrick <brian.patrick3@kyndryl.com>
2024-07-22 14:55:54 -0500

    re-export duotone

--- src/stories/Library.stories.js
+++ src/stories/Library.stories.js
@@ -1,7 +1,6 @@
 import { html } from 'lit';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import '@kyndryl-design-system/shidoka-foundation/components/card';
-import Categories from '../manifest/categories.json';
 import Icons from '../manifest/icons.json';
 
 export default {
@@ -11,29 +10,20 @@ export default {
 export const Library = {
   render: () => {
     return html`
-      <div class="categories">
-        ${Categories.map((Category) => {
+      <div class="icons">
+        ${Icons.map((Icon) => {
           return html`
-            <div class="category">
-              <div class="category-name kd-type--headline-08">${Category.name}</div>
-              <div class="icons">
-                ${Category.members.map((Member) => {
-                  const Icon = Icons.find((icon) => icon.name === Member);
-                  return html`
-                    <kd-card>
-                      <div class="icon">
-                        <div class="icon-name kd-type--ui-05">${Icon.friendly_name}</div>
-                        <div class="svg">
-                          ${Icon.duotone
-                            ? unsafeHTML(require(`../svg/duotone/${Icon.name}.svg`))
-                            : unsafeHTML(require(`../svg/monochrome/32/${Icon.name}.svg`))}
-                        </div>
-                      </div>
-                    </kd-card>
-                  `;
-                })}
+            <kd-card>
+              <div class="icon">
+                <div class="icon-name kd-type--ui-04">${Icon.friendly_name}</div>
+
+                <div class="svg">
+                  ${Icon.duotone
+                    ? unsafeHTML(require(`../svg/duotone/${Icon.name}.svg`))
+                    : unsafeHTML(require(`../svg/monochrome/32/${Icon.name}.svg`))}
+                </div>
               </div>
-            </div>
+            </kd-card>
           `;
         })}
       </div>
