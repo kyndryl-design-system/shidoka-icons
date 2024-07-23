8f7178982b4892e842dd063aca5b11fbf2c01285
Brian Patrick <brian.patrick3@kyndryl.com>
2024-07-23 14:42:24 -0500

    build cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,14 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
-const rename = require('gulp-rename');
 
 // clean dist folder
 const clear = () => {
   return gulp.src('dist', { read: false }).pipe(clean());
 };
 
-// copy and rename SVG files
+// copy SVG files
 const copy = () => {
-  return gulp
-    .src('src/svg/**/*.svg')
-    .pipe(
-      rename(function (path) {
-        const iconName = path.basename
-          .split('simple_icon_')
-          .join('')
-          .split('duotone_icon_')
-          .join('');
-        const iconDir = path.dirname;
-
-        path.dirname = iconDir;
-        path.basename = iconName;
-      })
-    )
-    .pipe(gulp.dest('dist/svg'));
+  return gulp.src('src/svg/**/*.svg').pipe(gulp.dest('dist/svg'));
 };
 
 exports.default = gulp.series(clear, copy);
