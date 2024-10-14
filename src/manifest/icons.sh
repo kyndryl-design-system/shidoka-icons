dab3b9e52bb489c1d15d5ab464df7353acfb7a99
tulasigudibanda <47306803+tulasigudibanda@users.noreply.github.com>
2024-10-14 06:50:02 -0500

    feat: 1939476 - new shidoka icons (#4)
    * feat: 1939476 - new shidoka icons
    
    * fix: removed extra space
    
    * fix: update 10 existing icons
    
    * chore: added and updated icons
    
    * chore: script to update manifest file
    
    * chore: added and updated icons
    
    ---------
    
    Co-authored-by: Tulasi Gudibanda <tulasi.gudibanda1@kyndryl.com>

---
+++ src/manifest/icons.sh
@@ -0,0 +1,46 @@
+# Run command to update manifest file(icons.sh) : npm run manifest
+
+# Note :  After running the script, you may need to make some manual adjustments to the output file based on UX specifications for certain icons
+#   Adjust 'name' field for the icons with uppercase letters in the filename.
+#   Adjust 'category' field
+#   Adjust 'duotone'  field
+
+#!/bin/bash
+
+testlist="updated-icons.txt"  # Input file : Update this file with the list of newly added icons. Note: File should end with an empty line.
+manifest_file="icons.json"  # Output file
+
+new_elements_array="[]"
+
+# Loop through each line in the testlist file
+while IFS= read -r file; do
+    # Check if the file variable is not empty
+    if [ -n "$file" ]; then
+        # Transform the file name to create the friendly_name and icon_name
+        icon_name=$(echo "$file" | tr '[:upper:]' '[:lower:]')  # Converts icon file name to lowercase.
+        friendly_name=$(echo "$file" | tr '-' ' ' | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) substr($i,2); print}')  # Transforms the file name into a more user-friendly format by replacing hyphens with spaces and capitalizing each word.
+
+        # Create new JSON element based on the current file name
+        new_element=$(jq -n --arg name "$file" --arg friendly_name "$friendly_name" --arg icon_name "$icon_name" '{
+            "name": $icon_name,
+            "friendly_name": $friendly_name,
+            "category": "Functional",
+            "aliases": [],
+            "duotone": false
+        }')
+
+        # Append the new element to the new_elements_array
+        new_elements_array=$(echo "$new_elements_array" | jq --argjson new_element "$new_element" '. += [$new_element]')
+    fi
+done < "$testlist"
+
+# Read existing JSON from the manifest file, default to an empty array if the file is empty
+existing_json=$(cat "$manifest_file" 2>/dev/null || echo "[]")
+
+# Combine existing JSON with new elements and sort
+combined_json=$(echo "$existing_json" "$new_elements_array" | jq -s 'add | sort_by(.name)')
+
+# Write the updated JSON back to the manifest file
+echo "$combined_json" > "$manifest_file"
+
+
