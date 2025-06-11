53ed39a4da1318443dc82688c7be85978c9caf93
Brian Patrick <brian.patrick3@kyndryl.com>
2025-06-11 10:15:20 -0500

    chore: storybook 9 upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,62 +1,16 @@
 /** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
+
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/addon-themes',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        scssBuildRule: {
-          test: /\.s(c|a)ss$/,
-          exclude: [/node_modules/],
-          oneOf: [
-            {
-              resourceQuery: /global/,
-              use: [
-                'style-loader',
-                'css-loader',
-                'resolve-url-loader',
-                {
-                  loader: 'sass-loader?sourceMap',
-                  options: {
-                    sourceMap: true,
-                  },
-                },
-              ],
-            },
-            {
-              use: [
-                {
-                  loader: 'lit-css-loader',
-                  options: {
-                    transform: (data, { filePath }) =>
-                      Sass.renderSync({ data, file: filePath }).css.toString(),
-                  },
-                },
-                'sass-loader',
-              ],
-            },
-          ],
-        },
-      },
-    },
-    {
-      name: 'storybook-preset-inline-svg',
-      options: {
-        svgInlineLoaderOptions: {
-          removeSVGTagAttrs: false,
-        },
-      },
-    },
-    '@storybook/addon-mdx-gfm',
-    '@storybook/addon-webpack5-compiler-babel',
+    '@storybook/addon-docs',
   ],
 
   framework: {
-    name: '@storybook/web-components-webpack5',
+    name: '@storybook/web-components-vite',
     options: {},
   },
 
@@ -68,4 +22,5 @@ const config = {
 
   docs: {},
 };
+
 export default config;
